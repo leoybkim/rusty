9240487103d0409d6ecee0191f06d69e1dbbc434
Leo Kim <leoybkim@gmail.com>
2026-02-22 23:36:11 -0500

    Rust by example: 12. Cargo

---
+++ rust_by_example/12_cargo/foo/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:warning=The build script is running right now!");
+}
---
+++ rust_by_example/12_cargo/foo/tests/my_other_test.rs
@@ -0,0 +1,14 @@
+#[test]
+fn test_bar() {
+    assert_eq!(true, true);
+}
+
+#[test]
+fn test_baz() {
+    assert_eq!(true, true);
+}
+
+#[test]
+fn test_foo_bar() {
+    assert_eq!(true, true);
+}
